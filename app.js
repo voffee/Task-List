bd4b625df743bf3a427744ec8b2ec1d5cc5a2f96
F Bliss <vanavil@fastmail.com>
2022-12-12 11:46:31 +0800

    Delete and Filter Tasks Completed

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+    // Removal task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -40,4 +46,45 @@ function addTask(e) {
     taskInput.value = '';
     
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        
+        // Confirm gives a pop up asking if you are sure
+        if (confirm('Are You Sure?')) {
+            e.target.parentElement.parentElement.remove();
+            //look into using closest method instead of searching parent/child
+        }
+        }
+        
+    
+}
+
+// Clear Tasks
+function clearTasks() {
+    // taskList.innerHTML = '';
+
+    // Faster
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+    
+    // https://jsperf.com/innerhtml-vs-removechild
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+    } else {
+        task.style.display = 'none';
+    }
+    });
+
 }
